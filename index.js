0e80da4da4c417cf553a894583273fffb140c138
Xiaoyi Liu <lxy878@gmail.com>
2020-08-11 12:36:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
-console.log(dodger.style.left); // "180px"
-console.log(dodger.style.bottom); // "0px"
