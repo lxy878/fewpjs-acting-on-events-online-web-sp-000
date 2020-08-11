8b99f616d27e6185fed5299f70380a5f2f8fc9b5
Xiaoyi Liu <lxy878@gmail.com>
2020-08-11 12:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }else if (e.key === 'ArrowRight'){
-    moveDodgerRight
+    moveDodgerRight();
   }
 });
 
