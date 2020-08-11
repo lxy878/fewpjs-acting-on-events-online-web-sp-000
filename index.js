65706b79c36f0c267c0f52988929b401e9a58bce
Xiaoyi Liu <lxy878@gmail.com>
2020-08-11 12:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
-
-    dodger.style.left = `${left - 1}px`;
+    moveDodgerLeft();
   }
 });
 
